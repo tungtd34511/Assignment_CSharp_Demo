d6e3dbe7611ce979fbcecb477e88441ca8b3defb
shanghaik168@gmail.com <Shanghaik1998>
2022-10-01 10:49:14 +0700

    abc

---
+++ AppViews/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using AppViews.Models;
+using Data.ModelsClass;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+namespace AppViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Sanpham> sanphams = new List<Sanpham>();
+            var httpClient = new HttpClient(); //tạo 1 http Client để call API
+            var response = await httpClient.GetAsync("https://localhost:7010/api/Sanphams/-get-all-sanpham");
+            // Lấy dữ liệu từ file Json - Cài nuget Newtonsoft.json
+            // Đọc ra 1 file Json
+            string sanphamResponse = await response.Content.ReadAsStringAsync();
+            // Lấy ra list object từ string json
+            sanphams = JsonConvert.DeserializeObject<List<Sanpham>>(sanphamResponse);
+            return View(sanphams);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- Assignment_CSharp_Demo/Controllers/SanphamsController.cs
+++ Assignment_CSharp_Demo/Controllers/SanphamsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Data.ModelsClass;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,11 +12,22 @@ namespace Assignment_CSharp_Demo_API.Controllers
     {
         // GET: api/<SanphamsController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        [Route("-get-all-sanpham")]
+        public IEnumerable<Sanpham> GetAllSanpham()
         {
-            return new string[] { "value1", "value2" };
+            // Fake data
+            List<Sanpham> sanpham = new List<Sanpham>()
+            {
+                new Sanpham{Id = Guid.NewGuid(), Gia = 10000, Ten = "Bim bim", Soluong = 22, Trangthai = true},
+                new Sanpham{Id = Guid.NewGuid(), Gia = 25000, Ten = "Coke", Soluong = 16, Trangthai = true},
+                new Sanpham{Id = Guid.NewGuid(), Gia = 50000, Ten = "Cà pháo", Soluong = 24, Trangthai = true},
+                new Sanpham{Id = Guid.NewGuid(), Gia = 14000, Ten = "Sữa", Soluong = 25, Trangthai = true},
+                new Sanpham{Id = Guid.NewGuid(), Gia = 25600, Ten = "Thịt", Soluong = 24, Trangthai = false},
+                new Sanpham{Id = Guid.NewGuid(), Gia = 25300, Ten = "Đậu", Soluong = 1000, Trangthai = true},
+                new Sanpham{Id = Guid.NewGuid(), Gia = 12345, Ten = "Gà", Soluong = 11, Trangthai = true}
+            };
+            return sanpham;
         }
-
         // GET api/<SanphamsController>/5
         [HttpGet("{id}")]
         public string Get(int id)
